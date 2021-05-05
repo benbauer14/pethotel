58f09f6697f482f50491cec1268e91c5951ad504
Pat Anderson <patrick.b.anderson@gmail.com>
2021-05-05 15:49:16 -0500

    added inputs and handler functions to AddPet

--- src/components/AddPet/AddPet.jsx
+++ src/components/AddPet/AddPet.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
 
 const AddPet = () => {
+    //to dispatch
+    const dispatch = useDispatch();
+    //set local state for pet object
+    const [ tempPet, setTempPet ] = useState({});
+
+    const handlePetName = ( event ) =>{
+        setTempPet( {...tempPet, pet: event.target.value } )
+    }
+
+    const handlePetColor = ( event ) =>{
+        setTempPet( {...tempPet, color: event.target.value } )
+    }
+
+    const handlePetBreed = ( event ) =>{
+        setTempPet( {...tempPet, breed: event.target.value } )
+    }
+
+    const handleOwner = ( event ) =>{
+        setTempPet( {...tempPet, owner: event.target.value } )
+    }
+
+
+
+    const addPet = ( petObject ) =>{
+        dispatch({ type: 'POST_NEWPET', payload: petObject })
+    }
+
+    //    { id: '1', owner: 'Chris', pet: 'Charlie', breed: 'Shih-tzu', color: 'Black', checkedin: 'no', actions: 'Delete | Check Out'},
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <input type="text" placeholder="Pet Name" onChange={handlePetName} />
+            <input type="text" placeholder="Pet Name" onChange={handlePetColor} />
+            <input type="text" placeholder="Pet Name" onChange={handlePetBreed} />
+            <select onChange={handleOwner}>
+                <option value=''></option>
+                <option>Joel</option>
+                <option>Ben</option>
+                <option>Pat</option>
+            </select>
+            <button onClick={()=>addPet( tempPet )}>Add</button>
+        </>
     )
 }
 
