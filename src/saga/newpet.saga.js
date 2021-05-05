5207ad4e757531a98a9ae220d54fa5a56db510ec
Ben Bauer <ben.bauer1@gmail.com>
2021-05-05 15:52:45 -0500

    updated put call in newpet

--- src/saga/newpet.saga.js
+++ src/saga/newpet.saga.js
@@ -5,12 +5,11 @@ import { put, takeLatest } from 'redux-saga/effects';
 function* postNewPet(action) {
 
   try {
-    yield axios.post('/api/chat/?pet=' + action.pet + '&owner=' + action.owner);
+    yield axios.post('/api/pet/', { owner: action.owner, pet: action.pet, breed: action.breed, color: action.color, checkedin: action.checkedin});
   } catch (error) {
     console.log('Pet POST request failed', error);
   }
 }
-
 
 function* newpetSaga() {
   yield takeLatest('POST_NEWPET', postNewPet);
