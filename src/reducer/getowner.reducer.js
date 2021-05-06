8e3cb8cd45dc953a7068feb16a8821bedaaca2f1
Joel Eckerson <joelaeckerson@gmail.com>
2021-05-06 13:03:22 -0500

    owner reducer

---
+++ src/reducer/getowner.reducer.js
@@ -0,0 +1,12 @@
+const pets = (state = {}, action) => {
+    switch (action.type) {
+      case 'SET_OWNERS':
+        console.log(action)
+        return action.payload;
+      default:
+        return state;
+    }
+  };
+  
+
+  export default pets;
