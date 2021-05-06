2d81744b642f2b2671d80033815753e39231355e
Pat Anderson <patrick.b.anderson@gmail.com>
2021-05-06 15:23:16 -0500

    trying to get delete saga talking to server

--- src/saga/delete.saga.js
+++ src/saga/delete.saga.js
@@ -5,7 +5,8 @@ import { put, takeLatest } from 'redux-saga/effects';
 function* deletePetSaga(action) {
 
   try {
-    yield axios.delete('/api/pets/', action.payload );
+    console.log( 'payload:', action.payload )
+    yield axios.delete('/api/pets/' + action.payload.id );
     yield put({ type:'FETCH_PET'})
   } catch (error) {
     console.log('Pet DELETE request failed', error);
