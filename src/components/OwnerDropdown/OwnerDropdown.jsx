299eb38b3eaf6a67e30760c94c8d7608c4862e45
Pat Anderson <patrick.b.anderson@gmail.com>
2021-05-06 11:42:24 -0500

    building out ownerdropdown

---
+++ src/components/OwnerDropdown/OwnerDropdown.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { 
+    FormControl,
+    InputLabel,
+    Select, 
+    } from '@material-ui/core/';
+
+const OwnerDropdown = ( { owners, handleOwner }) => {
+    return (
+        <>
+        <FormControl>
+            <InputLabel>Owner</InputLabel>
+            <Select onChange={handleOwner} color="secondary" className={classes.dropdown} label="state">
+                <option value='' />
+                { owners.map( ( owner )=> <option value={owner.id} key={owner.id} >{owner.name}</option>) }
+            </Select>
+        </FormControl>
+        </>
+    )
+}
+
+export default OwnerDropdown
