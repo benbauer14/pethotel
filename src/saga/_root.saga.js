73fd2fccead64e3c121e9ad49b4f1c0230527b56
Ben Bauer <ben.bauer1@gmail.com>
2021-05-06 11:48:38 -0500

    update sagas

--- src/saga/_root.saga.js
+++ src/saga/_root.saga.js
@@ -2,12 +2,16 @@ import { all } from 'redux-saga/effects';
 import newpetSaga from './newpet.saga'
 import pets from './pets.saga'
 import newOwnerSaga from './newowner.saga'
+import updatecheckedin from './updatecheckedin.saga'
+import getOwners from './getowner.saga'
 
 export default function* rootSaga() {
     yield all([
         newpetSaga(),
         pets(),
-        newOwnerSaga()
+        newOwnerSaga(), 
+        updatecheckedin(),
+        getOwners()
     ]);
   }
   
